fb936ae5254a9d9a8363c98f76f216b3a54b34c5
FARUK2059 <omarfaruk34552@gmail.com>
2024-03-01 23:30:16 +0600

    Phone Hunter Conplate

--- Phone-Project-API.js
+++ Phone-Project-API.js
@@ -1,14 +1,14 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText='13', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhones (phones);
+    displayPhones (phones, isShowAll);
 }
 
 
-const displayPhones = phones => {
-    console.log(phones);
+const displayPhones = (phones, isShowAll) => {
+    // console.log(phones);
     // step : 1 link of id
     const phoneContainer = document.getElementById('phone-container');
     // clear search button
@@ -16,18 +16,21 @@ const displayPhones = phones => {
 
     //  show all button condition
     const showAllContainer = document.getElementById('Show-All-Button');
-    if (phones.length > 12) {
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     }
     else{
         showAllContainer.classList.add('hidden');
     }
 
+    // console.log('is show all', isShowAll);
     // show display item
-    phones = phones.slice(0,12);
+    if (!isShowAll){
+        phones = phones.slice(0,12);
+    }
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // step: 2 creat a div 
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card w-96 p-4  bg-[#dbeafe] shadow-xl `;
@@ -37,8 +40,8 @@ const displayPhones = phones => {
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>${phone.slug}</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button onclick ="handelShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -53,16 +56,55 @@ const displayPhones = phones => {
 
 }
 
+// handel Show Ditails
+const handelShowDetails = async(id) => {
+    // console.log('clicked show details', id)
+    // load single Data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    // console.log(data);
+    const phone = data.data;
+
+    showDetails(phone);
+}
+
+const showDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('show-details-phone-name');
+    phoneName.innerText = phone.name;
+    
+
+    const showDetailsContainer = document.getElementById('show-detail-Container');
+    showDetailsContainer.innerHTML = `
+    <img class="m-auto p-4 text-center justify-center items-center" src="${phone.image}" alt="">
+    <p><span class="text-[black]"> Brand :</span> ${phone.brand}</p>
+    <p><span class="text-[black]"> Storage :</span> ${phone.mainFeatures.storage}</p>
+    <p><span class="text-[black]"> ChipSet:</span> ${phone.mainFeatures.chipSet}</p>
+    <p><span class="text-[black]"> DisplaySize:</span> ${phone.mainFeatures.displaySize}</p>
+    <p><span class="text-[black]"> memory:</span> ${phone.mainFeatures.memory}</p>
+    <p><span class="text-[black]"> Bluetooth: </span> ${phone.others?.Bluetooth || 'No Bluetooth Available'}</p>
+    <p><span class="text-[black]"> GPS: </span> ${phone.others?.GPS || 'No GPS Available'}</p>
+    <p><span class="text-[black]"> NFC: </span> ${phone.others?.NFC || 'No NFC Available'}</p>
+    <p><span class="text-[black]"> Radio: </span> ${phone.others?.Radio || 'No Radio Available'}</p>
+    <p><span class="text-[black]"> USB: </span> ${phone.others?.USB || 'No USB Available'}</p>
+    <p><span class="text-[black]"> releaseDate: </span> ${phone.others?.releaseDate || 'No releaseDate Available'}</p>
+    <p><span class="text-[black]"> WLAN: </span> ${phone.others?.WLAN || 'No WLAN Available'}</p>
+    
+    `;
+    
+    // show modal
+    modal.showModal();
+}
+
 
 // handel searsh button
-
-const handelSearch = () => {
+const handelSearch = (isShowAll) => {
     toggleLoaddingSpinner (true);
     // console.log('search handel')
     const searchHander = document.getElementById('search-Fild');
     const searchText = searchHander.value;
-    console.log(searchText);
-    loadPhone(searchText);
+    // console.log(searchText);
+    loadPhone(searchText, isShowAll);
 }
 
 // const handelSearch2 = () => {
@@ -82,5 +124,10 @@ const toggleLoaddingSpinner = (isLoading) => {
     }
 }
 
+// handel show all
+const handelShowAll = () => {
+    handelSearch (true);
+}
 
-// loadPhone ();
+
+loadPhone ();
